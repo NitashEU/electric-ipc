c83890398653aaa3978584bf70c41b073fcfa779
NitashEU <fs@nitash.de>
2020-01-26 17:46:41 +0100

    latest stage

--- src/electric-ipc.ts
+++ src/electric-ipc.ts
@@ -1,5 +1,4 @@
-import { BrowserWindow, ipcMain, ipcRenderer } from 'electron';
-import * as uuid from 'uuid';
+import { ipcMain, ipcRenderer } from 'electron';
 
 export class ElectricIPC {
   private readonly _propertyNamesToIgnore: string[] = [
@@ -10,10 +9,13 @@ export class ElectricIPC {
   ];
   private _prototype: any;
 
-  protected constructor(private _window?: BrowserWindow, ...propertyNamesToIgnore: string[]) {
+  protected constructor(...propertyNamesToIgnore: string[]) {
     this._propertyNamesToIgnore.push(...propertyNamesToIgnore);
 
     this._prototype = Object.getPrototypeOf(this);
+    const funcNameRegex = /function (.{1,})\(/;
+    const results = funcNameRegex.exec(this.constructor.toString());
+    this._prototype.name = results && results.length > 1 ? results[1] : '';
     const propertyNames = Object.getOwnPropertyNames(this._prototype);
 
     for (const propertyName of propertyNames) {
@@ -24,42 +26,22 @@ export class ElectricIPC {
       if (typeof property !== 'function') {
         continue;
       }
-      if (_window) {
-        this.setupMainListener(propertyName, (...args: any[]) => this[propertyName](...args));
+      const name = this._prototype.name + '.' + propertyName;
+      if (process && process.type !== 'renderer') {
+        this.setupMainListener(name, (...args: any[]) => this[propertyName](...args));
       } else {
-        this._prototype[propertyName] = (args: any[]) => this.setupRendererListener(propertyName, args);
+        this._prototype[propertyName] = (args: any[]) => ipcRenderer.invoke(name, args);
       }
     }
   }
 
   private setupMainListener(key: string, cb: (...args: any[]) => Promise<void | any>): void {
-    const idLessTopic = this._prototype.name + '.' + key;
-
-    ipcMain.on(idLessTopic, async (_event: unknown, id: string, args: any[]) => {
+    ipcMain.handle(key, async (_event, args) => {
       let data: void | any;
-
       try {
-        data = await cb(...args);
+        data = await cb(args);
       } catch (e) {}
-
-      this._window.webContents.send(idLessTopic + id, data);
-    });
-  }
-
-  private setupRendererListener(key: string, ...args: any[]): Promise<any | void> {
-    return new Promise<any | void>((resolve, reject) => {
-      try {
-        const id = uuid.v4();
-        const idLessTopic = this._prototype.name + '.' + key;
-        const listener = (_event: unknown, value: any) => {
-          ipcRenderer.removeListener(idLessTopic + id, listener);
-          return resolve(value);
-        };
-        ipcRenderer.on(idLessTopic + id, listener);
-        ipcRenderer.send(idLessTopic, id, args);
-      } catch (err) {
-        reject(err);
-      }
+      return data;
     });
   }
 }
